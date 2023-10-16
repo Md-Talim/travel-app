5c11fdf1e1977814fb9336ba4e75d63168d3e1cf
Mohammad Talim <talimmohammad116@gmail.com>
2023-10-16 22:12:54 +0530

    Build the navbar

---
+++ src/components/Button.tsx
@@ -0,0 +1,27 @@
+import Image from 'next/image';
+
+interface Props {
+  full?: boolean;
+  icon?: string;
+  title: string;
+  type: 'button' | 'submit';
+  variant: string;
+}
+
+const Button = ({ title, type, icon, variant, full }: Props) => {
+  return (
+    <button
+      className={`flexCenter gap-3 rounded-full border ${variant} ${
+        full && 'w-full'
+      }`}
+      type={type}
+    >
+      {icon && <Image src={icon} alt={title} height={24} width={24} />}
+      <label className="bold-26 whitespace-nowrap cursor-pointer">
+        {title}
+      </label>
+    </button>
+  );
+};
+
+export default Button;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,44 @@
+import { navLinks } from '@/constants';
+import Image from 'next/image';
+import Link from 'next/link';
+import Button from './Button';
+
 const Navbar = () => {
   return (
-    <nav className="">
-      <h1>Next Price</h1>
+    <nav className="flexBetween max-container padding-container relative z-30 py-5">
+      <Link href="/">
+        <Image src="/hilink-logo.svg" width={74} height={29} alt="logo" />
+      </Link>
+
+      <ul className="hidden h-full gap-12 lg:flex">
+        {navLinks.map((navLink) => (
+          <li key={navLink.key}>
+            <Link
+              href={navLink.href}
+              className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
+            >
+              {navLink.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+
+      <div className="hidden lg:flexCenter">
+        <Button
+          type="button"
+          title="Login"
+          icon="/user.svg"
+          variant="btn_dark_green"
+        />
+      </div>
+
+      <Image
+        src="menu.svg"
+        alt="menu"
+        width={24}
+        height={24}
+        className="inline-block cursor-pointer lg:hidden"
+      />
     </nav>
   );
 };
