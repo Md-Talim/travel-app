d81b694cf22cce818d046c53aa75b7e12b44b586
Mohammad Talim <talimmohammad116@gmail.com>
2023-10-17 21:12:04 +0530

    Build the Hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import Hero from '@/components/Hero';
+
 const Home = () => {
-  return <h1 className="text-5xl">Next Price</h1>;
+  return (
+    <>
+      <Hero />
+    </>
+  );
 };
 
 export default Home;
